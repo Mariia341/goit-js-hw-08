eb7c764e65ffceaf5b7400d3997f2741bd9faac4
Mariia Makhonina <106169802+Mariia341@users.noreply.github.com>
2022-12-27 02:45:48 +0200

    add hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
+
+
+import '../css/common.css';
+import '../css/01-gallery.css';
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
+
+console.log(SimpleLightbox);
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const imgGallery = CreateAndRenderMarkupGallery(galleryItems);
+gallery.insertAdjacentHTML('beforeend', imgGallery);
+function CreateAndRenderMarkupGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div >
+`;
+    })
+      .join('');
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {captionsData:"alt", captionDelay:250});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+const savedTim = localStorage.getItem('videoplayer-current-time');
+if (savedTim) {
+  console.log(savedTim);
+}
+
+player
+  .setCurrentTime(savedTim)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
